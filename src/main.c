f516e3e6cd669cafa20cc7a77b1eaae640495973
plaxer <141951215+plaaxer@users.noreply.github.com>
2024-05-25 13:40:16 -0300

    correcoes

--- src/main.c
+++ src/main.c
@@ -50,7 +50,7 @@ toy_t **init_toys(int number){
 
 // Inicia a instância dos funcionarios
 ticket_t ** init_tickets(int number){
-    ticket_t **tickets = malloc(number * sizeof(toy_t));
+    ticket_t **tickets = malloc(number * sizeof(ticket_t));;
     for (int i = 0; i < number; i++){
         tickets[i] = (ticket_t *) malloc(sizeof(ticket_t));
         tickets[i]->id = i + 1;
@@ -111,7 +111,7 @@ int main(int argc, char *argv[]){
     cli_args->n = _config.clients;
 
     // Inicializa os funcionarios da bilheteria.
-    ticket_t **tickets = init_tickets(_config.toys);
+    ticket_t **tickets = init_tickets(_config.tickets);
     ticket_args->tickets = tickets;
     ticket_args->n = _config.tickets;
     
