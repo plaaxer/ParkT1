de154720c579a342ac54c53e7c5af3bd1011d09c
gabriel-salmoria <gabrielsalmoria1@gmail.com>
2024-06-05 20:35:55 -0300

    minor chances, cleaning debug

--- src/client.c
+++ src/client.c
@@ -20,48 +20,36 @@ pthread_t *clients_t;
 
 void playing(toy_t *toy, int client_id) {
  
-  // ENTRA FILA DO BRINQUEDO
-  // Mutex para bloquear a condicao de corrida da inicializacao do brinquedo alem de esperar um cliente. Explicacao no toy.c
-  debug("[MUTEX] - Cliente [%d] travado no mutex_start do brinquedo [%d].\n", client_id, toy->id);
-  //pthread_mutex_lock(&toy->mutex_start);
-  //TEMP
   int sem_capacity_value;
   sem_getvalue(&toy->sem_capacity, &sem_capacity_value);
-  printf("The value of sem_capacity is: %d\n", sem_capacity_value);
 
-  debug("[SEM] - Cliente [%d] travado no sem_capacity do brinquedo [%d].\n", client_id, toy->id);
   sem_wait(&toy->sem_capacity);
-  //pthread_mutex_unlock(&toy->mutex_start);
 
-  // FALA QUE TEM ALGUEM
-  debug("[MUTEX] - Cliente [%d] travado no mutex_ready do brinquedo [%d].\n", client_id, toy->id);
-// Mutex para bloquear a condicao de corrida da inicializacao do brinquedo alem de esperar um cliente. Explicacao no toy.c
+  // Mutex para bloquear a condicao de corrida da inicializacao do brinquedo alem de esperar um cliente. Explicacao no toy.c
   pthread_mutex_lock(&toy->mutex_ready);
+
   // A variavel ready eh utilizada para evitar Spurious Wakeups, alem para outro caso explicado em toy.c.
   toy->ready = 1;
   // Sinaliza ao brinquedo que ha um cliente dentro dele e que ele poderia comecar a rodar.
   pthread_cond_broadcast(&toy->ready_to_start);
-  pthread_mutex_unlock(&toy->mutex_ready);
 
-    // ESPERA PODER COMECAR A SE DIVERTIR
-  debug("[WAITING] - Cliente [%d] esperando o brinquedo [%d] iniciar.\n", client_id, toy->id);
+  pthread_mutex_unlock(&toy->mutex_ready);
+  
   // Mutex condicional para que o cliente espere o brinquedo iniciar. While (!toy->running) para evitar Spurious Wakeups.
   pthread_mutex_lock(&toy->mutex_cond);
 
   toy->ready_clients++;
-  pthread_cond_signal(&toy->all_clients_ready);
-  while (!toy->running){
-    debug("[WAITING] - Cliente [%d] Esperando dentro do cond [%d]. Valor de ready_clients: [%d] \n", client_id, toy->id, toy->ready_clients);
-    pthread_cond_wait(&toy->cond, &toy->mutex_cond);
-    debug("[TEMP] - Cliente [%d] foi liberado temporariamente no toy [%d]\n", client_id, toy->id);
-  }
-  debug("[COND] - Cliente [%d] liberado para brincar no brinquedo [%d].\n", client_id, toy->id);
 
+  pthread_cond_signal(&toy->all_clients_ready);
+
+  while (!toy->running)
+    pthread_cond_wait(&toy->cond, &toy->mutex_cond);
+  
   pthread_mutex_unlock(&toy->mutex_cond);
 
   // BRINQUEDO FUNCIONANDO
 
-  debug("[PLAY] - Cliente [%d] brincando no brinquedo [%d].\n", toy->id, client_id);
+  debug("[PLAY] - Cliente [%d] brincando no brinquedo [%d].\n", client_id, toy->id);
   sleep(0); // duracao da brinquadeira
 
   pthread_mutex_lock(&toy->mutex_cond);
@@ -73,41 +61,36 @@ void playing(toy_t *toy, int client_id) {
 // Thread que implementa o fluxo do cliente no parque.
 void *enjoy(void *arg) {
 
-  client_t *client =
-      (client_t *)arg; // casting do argumento na struct do cliente
+  client_t *client = (client_t *)arg; // casting do argumento na struct do cliente
 
   queue_enter(client);
-  sleep(0);
-  debug("[ENTER] Turista [%d] entrou no parque com [%d] moedas.\n", client->id,
-        client->coins);
 
-  sleep(0);
 
+  debug("[ENTER] Turista [%d] entrou no parque com [%d] moedas.\n", client->id, client->coins);
+
+  // entra no parque
+  sleep(1);
+
+  // permanece no parque brincando ate acabar suas moedas.
   while (client->coins > 0) {
     int toy_id = rand() % client->number_toys;
     playing(client->toys[toy_id], client->id);
-    debug("Moedas restantes: %d\n", client->coins);
     client->coins--;
   }
 
-
-  sleep(0);
-
   debug("[EXIT] - O cliente [%d] saiu do parque.\n", client->id);
+
   pthread_exit(NULL);
 }
 
 // Funcao onde o cliente compra as moedas para usar os brinquedos
 void buy_coins(client_t *self) {
-
-  self->coins = rand() % (MAX_COINS - 1) + MIN_COINS;
-
+  self->coins = ( rand() % MAX_COINS ) + 1;
 }
 
-// Função onde o cliente espera a liberacao da bilheteria para adentrar ao
-// parque.
+// Função onde o cliente espera a liberacao da bilheteria para adentrar ao parque.
 void wait_ticket(client_t *self) {
-  // cliente vai ser travado ate que a bilheteria o libere //ALTERAR PARA SEMAFORO BINARIO DEPOIS
+  // cliente vai ser travado ate que a bilheteria o libere
   sem_wait(&(client_semaphores[self->id - 1]));
 }
 
@@ -142,8 +125,6 @@ void open_gate(client_args *args) {
   for (int i = 0; i < number_of_clients; i++)
     sem_init(&client_semaphores[i], 0, 0);
 
-
-  debug("[INIT] - Inicializando lista de mutex\n");
 
   // Cria N threads (N clientes)
   for (int i = 0; i < number_of_clients; i++) {
--- src/toy.c
+++ src/toy.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 
 #include "shared.h"
 #include "toy.h"
@@ -20,68 +21,61 @@ int ON = 1; // Variavel global para ligar/desligar os brinquedos
 
 // Thread que o brinquedo vai usar durante toda a simulacao do sistema
 void *turn_on(void *args) {
+  // Casting do argumento na struct do brinquedo
+  toy_t *toy = (toy_t *)args; 
 
-  toy_t *toy = (toy_t *)args; // Casting do argumento na struct do brinquedo
+  // Variavel para armazenar a quantidade de clientes que irao brincar
+  int current_clients = 0;
 
-  int current_clients = 0; // Variavel para armazenar a quantidade de clientes que irao brincar
-
-  int missing; // Variavel para armazenar a quantidade de clientes que faltam para atingir a capacidade maxima do brinquedo
-
-  pthread_mutex_init(&toy->mutex_start, NULL); // inicializa o mutex que controla o bloqueio quando o brinquedo roda
-  pthread_mutex_init(&toy->mutex_cond, NULL); // inicializa o mutex auxiliar para a variavel de condicao para evitar condicao de corrida
-  pthread_mutex_init(&toy->mutex_ready, NULL); // inicializa o mutex auxiliar para a variavel de condicao de start
-  sem_init(&toy->sem_capacity, 0, toy->capacity); // inicializa o semaforo que controla a capacidade do brinquedo
-
+  // Variavel para armazenar a quantidade de clientes que faltam para atingir a capacidade maxima do brinquedo
+  int missing;
+  
   debug("[ON] - O brinquedo  [%d] foi ligado.\n",
         toy->id);
 
   while (ON){
     
     // ESPERANDO CLIENTES
-    sleep(0);
+    sleep(1);
 
     // ANTES DE COMECAR A RODAR
 
     // Aqui ocorre a verificacao de se ha clientes no brinquedo. Se nao houver, o brinquedo nao pode rodar. Obviamente, nao ha busy waiting
     // pois o brinquedo espera por um sinal para comecar a rodar --> o toy->ready eh para evitar Spurious Wakeups.
-    debug("Brinquedo [%d] esta esperando gente\n", toy->id);
     pthread_mutex_lock(&toy->mutex_ready);
     while (!toy->ready) {
       pthread_cond_wait(&toy->ready_to_start, &toy->mutex_ready);
     }
     pthread_mutex_unlock(&toy->mutex_ready);
-    debug("Brinquedo [%d] ira fechar\n", toy->id);
     // Aqui, pegamos o valor de quantos clientes faltam para a capacidade maxima e preenchemos o semaforo para que nenhum cliente
     // entre enquanto o brinquedo roda. O mutex eh para que nenhum cliente entre, o que possivelmente tornaria o valor de getValue
     // incorreto.
-    //pthread_mutex_lock(&toy->mutex_start);
-    debug("[TOY] - Brinquedo [%d] fechando o brinquedo.\n", toy->id);
+
     sem_getvalue(&toy->sem_capacity, &missing);
     for (int i = 0; i < missing; i++) {
       sem_wait(&toy->sem_capacity);
     }
-    debug("[TOY] - Brinquedo [%d] fechou o brinquedo. [%d] lugares restantes.\n", toy->id, missing);
     current_clients = toy->capacity - missing;
-    //pthread_mutex_unlock(&toy->mutex_start);
 
     // COMECA A RODAR
+
     // Aqui bloqueamos o mutex_condicional para poder atualizarmos a variavel de condicao e liberar os clientes que estavam esperando
     // o brinquedo rodar. Alem disso setamos a variavel auxiliar running para 1, indicando que o brinquedo esta rodando. A running eh
     // utilizada para evitar Spurious Wakeups.
 
     pthread_mutex_lock(&toy->mutex_cond);
-    debug("[TOY] - Brinquedo [%d] testando. Current clients: [%d]. Valor de running: [%d]. Valor de ready_clients: [%d]\n", toy->id, current_clients, toy->running, toy->ready_clients);
-    while (toy->ready_clients < current_clients){
+
+    while (toy->ready_clients < current_clients)
       pthread_cond_wait(&toy->all_clients_ready, &toy->mutex_cond);
-    }
-    debug("[TOY] - Brinquedo [%d} liberou todos os clientes para brincar.\n", toy->id);
+    
     toy->ready_clients = 0;
     toy->running = 1;
     pthread_cond_broadcast(&toy->cond);
     pthread_mutex_unlock(&toy->mutex_cond);
 
     // RODANDO
-    debug("[TOY] - Brinquedo [%d] rodando --> [%d] lugares restantes --> [%d] capacidade maxima\n", toy->id, missing, toy->capacity);
+
+    debug("[TOY] - Brinquedo [%d] rodando, foram ocupados [%d/%d] lugares.\n", toy->id, missing, toy->capacity);
     sleep(0);
 
     // ESPERA PARA N TERMINAR COM ALGUEM PRESO
@@ -91,7 +85,6 @@ void *turn_on(void *args) {
     }
     toy->done_clients = 0;
     pthread_mutex_unlock(&toy->mutex_cond);
-    debug("[TOY] - Brinquedo [%d] terminou de rodar.\n", toy->id);
 
     // Aqui setamos a variavel running para 0 (brinquedo parou de rodar). Bloqueamos o mutex_start para que nenhum cliente entre (pela
     // mesma razao do inicio) e liberamos os semaforos para que os clientes possam entrar (semaforo valera a capacidade do brinquedo).
@@ -103,30 +96,41 @@ void *turn_on(void *args) {
   }
 
 
-  debug("[OFF] - O brinquedo [%d] foi desligado.\n",
-        toy->id);
+  debug("[OFF] - O brinquedo [%d] foi desligado.\n", toy->id);
 
   pthread_exit(NULL);
 }
 
 
-
-
-
 // Essa função recebe como argumento informações e deve iniciar os brinquedos.
 void open_toys(toy_args *args) {
+  // guardamos as informações do brinquedos 
   toys_info = args;
 
   // Cria N threads (N brinquedos)
-  for (int i = 0; i < args->n; i++) 
-    pthread_create(&(args->toys[i]->thread), NULL, turn_on, args->toys[i]);
+  for (int i = 0; i < toys_info->n; i++){
+    toy_t* toy = toys_info->toys[i];
 
+    pthread_mutex_init(&toy->mutex_start, NULL);    // inicializa o mutex que controla o bloqueio quando o brinquedo roda
+    pthread_mutex_init(&toy->mutex_cond, NULL);     // inicializa o mutex auxiliar para a variavel de condicao para evitar condicao de corrida
+    pthread_mutex_init(&toy->mutex_ready, NULL);    // inicializa o mutex auxiliar para a variavel de condicao de start
+    sem_init(&toy->sem_capacity, 0, toy->capacity); // inicializa o semaforo que controla a capacidade do brinquedo
+    
+    pthread_create(&(toy->thread), NULL, turn_on, toy);
+  }
 }
 
 
 // Função para desligar os brinquedos
 void close_toys() {
+  // sinalização para as threads pararem seu loop de execução.
   ON = 0;
+
+ // força o término da thread ao final da execução do código.
+  for (int i = 0; i < toys_info->n; i++) 
+    pthread_cancel(toys_info->toys[i]->thread);
+
+  debug("[CLOSE] Os brinquedos foram finalizados!\n");
 }
 
 // Desligando os brinquedos
